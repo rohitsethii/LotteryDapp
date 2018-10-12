6829342ba4d80d3d28723dd13c8ca0cba3a40dee
rohitsethii <rohitsethi836@gmail.com>
2018-10-12 21:38:04 +0530

    updated

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.22;
+pragma solidity ^0.4.25;
 
 contract Lottery{
     
@@ -8,17 +8,16 @@ contract Lottery{
     address[] public allholders;
     address[] public winners;
     
-    uint price=1000000 wei;
+    uint price;
     uint[] public allguess;
-    uint count=0;
+    uint count;
     uint public w;
     uint public Total;
-    uint playerID = 0;
-    //uint round = 3 minutes;
-    //uint startround = now + round;
+    uint playerID;
     
-    bool winnerDrawn = false;
-    bool active = true;
+    
+    bool winnerDrawn;
+    bool active;
     
     
 //-----------------------------------------Mappings----------------------------------------
@@ -48,19 +47,19 @@ contract Lottery{
     
     
     modifier onlyowner(){
-        require(msg.sender==owner);
+        require(msg.sender==owner,"only owner can call");
         _;
     }
     modifier notowner(){
-        require(msg.sender!=owner);
+        require(msg.sender!=owner,"owner is not allowed");
         _;
     }
     modifier alreadyDrawn(){
-        require(!winnerDrawn);
+        require(!winnerDrawn,"winner has already Drawn");
         _;
     }
     modifier LotteryActive{
-        require(active==true);
+        require(active==true,"lottery is not open");
         _;
     }
     
@@ -71,76 +70,67 @@ contract Lottery{
     
     
     constructor() public{
-        owner=msg.sender;
-    //bytes32  winninghash= keccak256(abi.encode(winninghash));
+        owner = msg.sender;
+        price = 1000000 wei;
+        active = true;
     }
+
     
+//------------------------------------Public Functions----------------------------------------------------       
+
+
     function makeGuess(uint guess) 
+    public
     notowner
-    LotteryActive
-    public 
+    LotteryActive 
     payable 
     returns(bool){
-        require(guess != 0x20);
-        require(guess<=20);
-        require(msg.value == price);
+        require(guess != 0x20,"cannot be empty");
+        require(guess<=20,"number should be between 0 and 20");
+        require(msg.value == price,"price should be 1000000");
         allholders.push(msg.sender);
-        holders[playerID]= msg.sender;
-        emit Guess(msg.sender,playerID,guess,msg.value);
+        holders[playerID] = msg.sender;
         playerID++;
         allguess.push(guess);
-        Guesses[msg.sender]= guess;
-        Total=Total + msg.value;
+        Guesses[msg.sender] = guess;
+        Total += msg.value;
+        emit Guess(msg.sender,playerID,guess,msg.value);
         return true;
-    }
-       
-       
-//------------------------------------Public Functions----------------------------------------------------       
-       
+    }       
       
-       
-        function drawwinner()
-        public 
-        onlyowner
-        alreadyDrawn
-        returns(address[]) {
-            
-            w=uint8(uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty)))%21);
-            for(uint i=0;i<allholders.length;i++){
-                if( w==allguess[i] ){
-                    winners.push(allholders[i]);
-                    count++;
-                }
+    
+    function drawwinner()
+    public 
+    onlyowner
+    alreadyDrawn
+    returns(address[]) {
+        
+        w = uint8(uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty)))%21);
+        for(uint i = 0; i < allholders.length; i++){
+            if( w == allguess[i] ){
+                winners.push(allholders[i]);
+                count++;
             }
-            winnerDrawn=true;
-            return winners;
         }
+        winnerDrawn = true;
+        return winners;
+    }
+    
+    
         
-        
-        
-        function distributeReward()
-        public
-        onlyowner
-        LotteryActive
-        {
-            if(count == 0){
-                selfdestruct(owner);
-            }
-            uint winnerreward= (address(this).balance/2)/count;
-            for(uint j=0;j<winners.length;j++){ 
-                winners[j].transfer(winnerreward);
-                
-            }
+    function distributeReward()
+    public
+    onlyowner
+    LotteryActive
+    {
+        if(count == 0){
             selfdestruct(owner);
-            
-            //    owner.transfer(address(this).balance);
-            //    if (address(this).balance == 0){
-            //        winnerDrawn = false;
-            //        active = true;
-            //        delete w;
-            //        delete Total;
-            //        delete allholders;
-            //   }
+        }
+        uint winnerreward = (address(this).balance/2)/count;
+        for(uint j = 0;j < winners.length;j++){ 
+            winners[j].transfer(winnerreward);
             
         }
-     }
+        selfdestruct(owner);
+    }
+}
