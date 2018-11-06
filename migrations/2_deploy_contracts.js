ff3fdd5ce8bef78abf22d6a3db287908fabd904b
Appinventiv <rohit.sethi@appinventiv.com>
2018-11-06 12:22:57 +0530

    initial migration to deploy_contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
+var Rlottery = artifacts.require("./Rlottery.sol");
 var Lottery = artifacts.require("./Lottery.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Lottery);
+  deployer.deploy(Rlottery);
 };
--- migrations/3_initial_migration.js
+++
@@ -1,5 +0,0 @@
-var Rlottery = artifacts.require("./Rlottery.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Rlottery);
-};
